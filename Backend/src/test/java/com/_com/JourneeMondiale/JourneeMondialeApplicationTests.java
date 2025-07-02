6b0a9085f6d2c29b9fe289d8f6b1e5c088039dc0
ABOULHAJ YASSINE <aboulhajyassine@gmail.com>
2025-07-02 17:08:55 +0100

    Initial commit

---
+++ Backend/src/test/java/com/_com/JourneeMondiale/JourneeMondialeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._com.JourneeMondiale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneeMondialeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
