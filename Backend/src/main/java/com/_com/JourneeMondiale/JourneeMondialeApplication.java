a1321f14036fcdf9f2ecb954f85ee196734efcaa
ABOULHAJ YASSINE <aboulhajyassine@gmail.com>
2025-07-02 17:08:55 +0100

    Initial commit

---
+++ Backend/src/main/java/com/_com/JourneeMondiale/JourneeMondialeApplication.java
@@ -0,0 +1,13 @@
+package com._com.JourneeMondiale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneeMondialeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneeMondialeApplication.class, args);
+	}
+
+}
